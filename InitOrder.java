ec3df9d3f00b2cec108ea6738b086ebb5be1574a
alex <klepa_82@bk.ru>
2023-08-15 18:23:25 +0300

    add 1

---
+++ InitOrder.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+/**
+ * Консольный вариант получения данных для создания Order
+ */
+public class InitOrder {
+    private Scanner scanner = new Scanner(System.in);
+
+    private String name;
+    private String product;
+    private int qnt;
+    private Double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public int getQnt() {
+        return qnt;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void inputFromConsole(){
+        name = processInput("Имя клиента: ");
+        product = processInput("Продукт: ");
+        qnt = Integer.parseInt(processInput("Кол-во: "));
+        price = Double.parseDouble(processInput("Цена: "));
+    }
+
+    private String processInput(String message){
+        System.out.print(message);
+        return scanner.nextLine();
+    }
+
+    @Override
+    public String toString() {
+        return "InitOrder{" +
+                "name='" + name + '\'' +
+                ", product='" + product + '\'' +
+                ", qnt=" + qnt +
+                ", price=" + price +
+                '}';
+    }
+}
